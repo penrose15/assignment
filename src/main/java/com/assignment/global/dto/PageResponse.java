9bbecd88d664a335a6e84eb103de273d010ff3fb
penrose15 <seongju99@naver.com>
2025-01-16 17:45:00 +0900

    feat: create rest config

---
+++ src/main/java/com/assignment/global/dto/PageResponse.java
@@ -0,0 +1,34 @@
+package com.assignment.global.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PageResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+    private boolean first;
+
+    public PageResponse(Page<T> page) {
+        this.content = page.getContent();
+        this.page = page.getNumber();
+        this.size = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.last = page.isLast();
+        this.first = page.isFirst();
+    }
+
+    public static <T> PageResponse<T> from(Page<T> page) {
+        return new PageResponse<>(page);
+    }
+}
